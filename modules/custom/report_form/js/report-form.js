9eb74836651654393c77eccc744aaab1e342a3e3
Pavlik Var <varaksib.soft.group@gmail.com>
2017-08-26 11:54:36 +0300

    first fix git settings

--- modules/custom/report_form/js/report-form.js
+++ modules/custom/report_form/js/report-form.js
@@ -1,9 +1,10 @@
 (function ($) {
-  Drupal.behaviors.ReportForm= {
+  Drupal.behaviors.ReportForm = {
     attach: function (context, settings) {
       try {
         $("select.reporter-list").msDropDown();
-      } catch(e) {
+      }
+      catch (e) {
         alert(e.message);
       }
     }
